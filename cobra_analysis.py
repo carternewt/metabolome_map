528317114f9149a735768061497bcc96dee21bdd
carternewt <150153088+carternewt@users.noreply.github.com>
2026-03-11 16:32:56 -0400

    Update cobra_analysis.sh & map.sh & create GG_medium.toml: Setting up script to utilize GG media for metabolic mapping instead

--- cobra_analysis.py
+++ cobra_analysis.py
@@ -2,15 +2,26 @@ import cobra
 import pandas as pd
 from cobra.flux_analysis import flux_variability_analysis
 
-model_path = "/work/lylab/cjn40747/metabolome/F7_5.xml"
+model_path = "/work/lylab/cjn40747/metabolome/F7_5_GG.xml"
 model = cobra.io.read_sbml_model(model_path)
 
-def set_lb_medium(model, glucose=False):
-    medium = model.medium.copy()
-    if "EX_glc__D_e" in model.reactions:
+GG_medium = {
+    "EX_glc__D_e": 55.5,
+    "EX_glu__L_e": 34.0,
+    "EX_mg2_e": 0.811,
+    "EX_so4_e": 0.811,
+    "EX_k_e": 3.67,
+    "EX_pi_e": 3.67,
+    "EX_na1_e": 3.42,
+    "EX_cl_e": 3.42,
+    "EX_ca2_e": 49.9,
+    "EX_h_e": 1000
+}
+
+def set_GG_medium(model, include_glucose=True):
+    medium = GG_medium_template.copy()
+    if not include_glucose:
         medium["EX_glc__D_e"] = 0
-    if glucose:
-        medium["EX_glc__D_e"] = 10
     model.medium = medium
 
 def find_secretions(model):
@@ -33,19 +44,19 @@ def find_secretions(model):
 if __name__ == "__main__":
     model = cobra.io.read_sbml_model(model_path)
     with model:
-        set_lb_medium(model, glucose=False)
-        lb_secretions = find_secretions(model)
+        set_GG_medium(model, include_glucose=False)
+        secretions_no_glc = find_secretions(model)
 
     with model:
-        set_lb_medium(model, glucose=True)
-        glc_secretions = find_secretions(model)
+        set_GG_medium(model, include_glucose=True)
+        secretions_with_glc = find_secretions(model)
 
     df = pd.DataFrame({
-        "LB_max_secretion": lb_secretions,
-        "LB_glucose_max_secretion": glc_secretions
+        "GG_max_secretion": secretions_no_glc,
+        "GG_glucose_max_secretion": secretions_with_glc
     }).fillna(0)
 
-    df["difference"] = df["LB_glucose_max_secretion"] - df["LB_max_secretion"]
+    df["difference"] = df["GG_glucose_max_secretion"] - df["GG_max_secretion"]
 
     df = df.sort_values("difference", ascending=False)
 
@@ -54,8 +65,8 @@ if __name__ == "__main__":
 
     df = df[[
         "metabolite_name",
-        "LB_max_secretion",
-        "LB_glucose_max_secretion",
+        "GG_max_secretion",
+        "GG_glucose_max_secretion",
         "difference"
     ]]
 
