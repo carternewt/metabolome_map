cbcfa384fb4d1a2e0e29db3c645b1b0dba9da988
carternewt <150153088+carternewt@users.noreply.github.com>
2026-03-11 12:32:52 -0400

    Update map.sh: Add code for CarveMe software

---
+++ map.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --time=1-00:00:00
+#SBATCH --mail-user=carter.newton@uga.edu
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --error=/work/lylab/cjn40747/metabolome/logs/%j.err
+#SBATCH --output=/work/lylab/cjn40747/metabolome/logs/%j.out
+
+OUT='/work/lylab/cjn40747/metabolome'
+HOME='/home/cjn40747/metabolome_map'
+
+ml purge
+ml CarveMe/1.6.6
+
+carve -o $OUT -g LB -v $OUT/F7_5.faa
