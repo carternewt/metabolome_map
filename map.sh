23c13e952297ebea156a2263b25cf4259926e105
carternewt <150153088+carternewt@users.noreply.github.com>
2026-03-11 17:03:03 -0400

    Update map.sh: troubleshooting

--- map.sh
+++ map.sh
@@ -15,7 +15,7 @@ HOME='/home/cjn40747/metabolome_map'
 ml purge
 ml CarveMe/1.6.6
 
-carve -o $OUT/F7_5.xml -v -g GG --mediadb GG_medium.tsv --fbc2 $OUT/F7_5.faa
+carve -o $OUT/F7_5.xml -v -g GG -i GG --mediadb GG_medium.tsv --fbc2 $OUT/F7_5.faa
 
 ml purge 
 
