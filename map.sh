00a154c5b2ceaa39a9a5c1e28b68ce5e13234b2b
carternewt <150153088+carternewt@users.noreply.github.com>
2026-03-20 14:13:32 -0400

    Update map.sh: troubleshooting

--- map.sh
+++ map.sh
@@ -12,14 +12,14 @@
 OUT='/work/lylab/cjn40747/metabolome'
 HOME='/home/cjn40747/metabolome_map'
 
-ml purge
-ml CarveMe/1.6.6
-source activate cobra
-
 find $OUT/type_strains -name *.faa -type f | while read -r file; do
+    ml purge
+    ml CarveMe/1.6.6
     base=$(basename "$file" .faa)
     carve -o $OUT/"$base"_GG.xml -v -g GG --mediadb $HOME/GG_medium.tsv --fbc2 $file
     carve -o $OUT/"$base"_CDB.xml -v -g CDB --mediadb $HOME/GG_medium.tsv --fbc2 $file
+    ml purge 
+    source activate cobra
     python cobra_analysisGG.py $OUT/"$base"_GG.xml
     python cobra_analysisGG.py $OUT/"$base"_CDB.xml
 done
